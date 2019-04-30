98b3e767cd695b58e68aa3f11ff474cae269f0ae
George Lie <glie2@illinois.edu>
2019-04-30 02:38:31 -0500

    GameActivity: added Jackpot counter and text, error message placeholder, submission button, input box

--- app/src/main/java/com/example/freedomofspeech/GameActivity.java
+++ app/src/main/java/com/example/freedomofspeech/GameActivity.java
@@ -3,6 +3,10 @@ package com.example.freedomofspeech;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -11,9 +15,37 @@ import com.google.gson.JsonParser;
 
 public class GameActivity extends AppCompatActivity {
 
+    TextView mainWord;
+    TextView jackpotNumber;
+    TextView errorMessage;
+    EditText guessBox;
+    Button submitButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        mainWord = findViewById(R.id.mainWord);
+        jackpotNumber = findViewById(R.id.jackpotNumber);
+        errorMessage = findViewById(R.id.errorMessage);
+        guessBox = findViewById(R.id.guessBox);
+        submitButton = findViewById(R.id.submitButton);
+
+        //Initial State
+        jackpotNumber.setText("1");
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /**
+                 * When the submit button is clicked :
+                 *
+                 * 1) call Dictionary method to check if word is valid or not
+                 * 2) If valid, increase jackpot counter and move onto next player screen
+                 * 3) If invalid, display error message and refresh screen
+                 */
+            }
+        });
     }
 }
--- app/src/main/java/com/example/freedomofspeech/MainActivity.java
+++ app/src/main/java/com/example/freedomofspeech/MainActivity.java
@@ -11,26 +11,30 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button startButton;
+    TextView numberOfPlayers;
+    SeekBar seekBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button startButton = findViewById(R.id.startButton);
-        final TextView resultsTextView = findViewById(R.id.resultsTextView);
-        SeekBar seekBar = findViewById(R.id.seekBar);
+        startButton = findViewById(R.id.startButton);
+        numberOfPlayers = findViewById(R.id.numberOfPlayers);
+        seekBar = findViewById(R.id.seekBar);
 
         /**
          * SeekBar : Dotted scroll bar for user to select the desired number of players
          */
-        resultsTextView.setText("1");
+        numberOfPlayers.setText("1");
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 
                 int numPlayers = progress + 1;
                 String numPlayersStr = Integer.toString(numPlayers);
-                resultsTextView.setText(numPlayersStr);
+                numberOfPlayers.setText(numPlayersStr);
             }
 
             @Override
