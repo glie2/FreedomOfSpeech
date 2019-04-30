88c6128d983df30e7fe989efeeff1951afa064bc
George Lie <glie2@illinois.edu>
2019-04-30 00:00:23 -0500

    Progress:
    Initial Menu Screen w/ basic buttons
    
    SeekBar to go from 1-8 players

--- app/src/main/java/com/example/freedomofspeech/MainActivity.java
+++ app/src/main/java/com/example/freedomofspeech/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.freedomofspeech;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button startButton = findViewById(R.id.startButton);
+        final TextView resultsTextView = findViewById(R.id.resultsTextView);
+        SeekBar seekBar = findViewById(R.id.seekBar);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                int numPlayers = progress + 1;
+                resultsTextView.setText("" + numPlayers + "");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
