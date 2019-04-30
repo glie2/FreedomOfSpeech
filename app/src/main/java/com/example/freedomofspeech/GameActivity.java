2d218ae090164f89d9e261c1470aa73cf61d20aa
Daniel Wu <dxwu2@illinois.edu>
2019-04-30 00:53:28 -0500

    dependencies

--- app/src/main/java/com/example/freedomofspeech/GameActivity.java
+++ app/src/main/java/com/example/freedomofspeech/GameActivity.java
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
 public class GameActivity extends AppCompatActivity {
 
     @Override
