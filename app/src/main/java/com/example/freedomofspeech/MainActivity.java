14d52b0820a35a07c43e8e7e33edb117d01568c6
George Lie <glie2@illinois.edu>
2019-05-01 09:30:33 -0500

    Made the whole thing look bomb! As in I made a f***ing dark mode and other stuff

--- app/src/main/java/com/example/freedomofspeech/MainActivity.java
+++ app/src/main/java/com/example/freedomofspeech/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.freedomofspeech;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.SeekBar;
+import android.widget.Switch;
 import android.widget.TextView;
 import java.util.ArrayList;
 
@@ -17,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     Button rulesButton;
     TextView numberOfPlayers;
     SeekBar seekBar;
+    Switch themeSwitch;
 
     static int[] scoreBoard = new int[8];
     static String starterWords[] = {"stress", "toys", "scant", "answer", "trap",
@@ -34,15 +38,39 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        if (AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES) {
+            setTheme(R.style.darktheme);
+        } else {
+            setTheme(R.style.AppTheme);
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        themeSwitch = findViewById(R.id.themeSwitch);
         usedWords = new ArrayList<>();
         startButton = findViewById(R.id.startButton);
         rulesButton = findViewById(R.id.rules_button);
         numberOfPlayers = findViewById(R.id.numberOfPlayers);
         seekBar = findViewById(R.id.seekBar);
 
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+            themeSwitch.setChecked(true);
+        }
+        themeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    restartApp();
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    restartApp();
+                }
+            }
+        });
         /**
          * SeekBar : Dotted scroll bar for user to select the desired number of players
          */
@@ -83,5 +111,11 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, RulesActivity.class));
             }
         });
+    }
+
+    public void restartApp() {
+        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(i);
+        finish();
     }
 }
