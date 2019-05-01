dd6dd7aea31de5bad0f8c9dac664c70db1787ba3
Daniel Wu <dxwu2@illinois.edu>
2019-05-01 05:58:34 -0500

    scoreboard algorithm

--- app/src/main/java/com/example/freedomofspeech/ScoreBoardActivity.java
+++ app/src/main/java/com/example/freedomofspeech/ScoreBoardActivity.java
@@ -3,10 +3,12 @@ package com.example.freedomofspeech;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class ScoreBoardActivity extends AppCompatActivity {
 
+    // text boxes for the scoreboard
     TextView scoreTitle;
     TextView p1score;
     TextView p2score;
@@ -22,6 +24,10 @@ public class ScoreBoardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score_board);
 
+        // the number of players participating
+        int numberOfPlayers = MainActivity.numPlayers;
+
+        // variables for the text boxes on the scoreboard
         scoreTitle = findViewById(R.id.scoreTitle);
         p1score = findViewById(R.id.player1score);
         p2score = findViewById(R.id.player2score);
@@ -31,6 +37,40 @@ public class ScoreBoardActivity extends AppCompatActivity {
         p6score = findViewById(R.id.player6score);
         p7score = findViewById(R.id.player7score);
         p8score = findViewById(R.id.player8score);
+
+        p1score.setText("Player 1 score: " + Integer.toString(MainActivity.scoreBoard[0]));
+        p2score.setText("Player 2 score: " + Integer.toString(MainActivity.scoreBoard[1]));
+        p3score.setText("Player 3 score: " + Integer.toString(MainActivity.scoreBoard[2]));
+        p4score.setText("Player 4 score: " + Integer.toString(MainActivity.scoreBoard[3]));
+        p5score.setText("Player 5 score: " + Integer.toString(MainActivity.scoreBoard[4]));
+        p6score.setText("Player 6 score: " + Integer.toString(MainActivity.scoreBoard[5]));
+        p7score.setText("Player 7 score: " + Integer.toString(MainActivity.scoreBoard[6]));
+        p8score.setText("Player 8 score: " + Integer.toString(MainActivity.scoreBoard[7]));
+
+
+
+        // set scoreboard according to how many people are playing
+        if (numberOfPlayers < 8) {
+            p8score.setVisibility(View.INVISIBLE);
+        }
+        if (numberOfPlayers < 7) {
+            p7score.setVisibility(View.INVISIBLE);
+        }
+        if (numberOfPlayers < 6) {
+            p6score.setVisibility(View.INVISIBLE);
+        }
+        if (numberOfPlayers < 5) {
+            p5score.setVisibility(View.INVISIBLE);
+        }
+        if (numberOfPlayers < 4) {
+            p4score.setVisibility(View.INVISIBLE);
+        }
+        if (numberOfPlayers < 3) {
+            p3score.setVisibility(View.INVISIBLE);
+        }
+        if (numberOfPlayers < 2) {
+            p2score.setVisibility(View.INVISIBLE);
+        }
     }
 
 
